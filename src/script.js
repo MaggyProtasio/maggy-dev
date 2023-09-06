ee10fa284d735baae2a345e059660532aed9b394
MaggyProtasio <mxggyprotasio@gmail.com>
2023-09-06 17:49:23 +0200

    FIX: circular pointer bug

--- src/script.js
+++ src/script.js
@@ -1,9 +1,19 @@
 //Custom circular mouse pointer
-$(document).ready(function() {
+/* $(document).ready(function() {
+    var cursor = $('#circular-cursor');
+
     $(document).on('mousemove', function(e) {
-      $('#circular-cursor').css({
+      cursor.css({
         left: e.pageX,
         top: e.pageY
       });
-    })
-  });
+    });
+
+    // Update cursor position on scroll
+    $(window).on('scroll', function() {
+      // Use a fixed value to update the top position
+      cursor.css({
+        top: 100  // Adjust this value as needed to keep the cursor visible
+      });
+    });
+});  */
